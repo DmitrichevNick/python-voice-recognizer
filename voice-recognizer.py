9f628a6229fbaad092e2ae3c78dd4a4be7a7f6eb
Dmitrichev Nick <dmitrichev.nick@gmail.com>
2020-08-12 20:20:03 +0300

    reworked thread

--- voice-recognizer.py
+++ voice-recognizer.py
@@ -3,48 +3,44 @@ import time
 import codecs
 import speech_recognition as sr
 
-subtitles = ""
-class WriterThread(Thread):
+messages_stack = []
+path = ""
+language = "ru-RU"
+
+class ListenerThread(Thread):
+    global messages_stack
+    voice_recognizer = None
     def __init__(self, name):
-        print("create")
+        print("listener created")
         Thread.__init__(self)
         self.name = name
+        self.voice_recognizer = sr.Recognizer()
+        self.voice_recognizer.non_speaking_duration = 0.001
+        self.voice_recognizer.pause_threshold = 0.001
+        self.voice_recognizer.phrase_threshold = 0.001
     
     def run(self):
-        messages = []
-        global subtitles
-
         while True:
-            time.sleep(0.1)
-            if len(subtitles) > 0 :
-                messages.insert(len(messages), "   "+subtitles+"\n")
-                if len(messages) == 4:
-                    messages.pop(0)
-                print("write")
-                f = codecs.open("path of govno", "w", "utf-8")
-                f.writelines(messages)
-                f.close()
-                subtitles = ""
+            with sr.Microphone() as source:
+                audio_text = self.voice_recognizer.listen(source)
+
+                try:
+                    text = self.voice_recognizer.recognize_google(audio_text, language=language)
+                    if bool(text):
+                        messages_stack.append(text)
+                        write_text(messages_stack.pop())
+                        print(text)
+                except Exception as exception:
+                    nope = 0
+                    #print(str(exception))
 
 def create_threads(): 
-    name = "WriterThread"
-    thread = WriterThread(name)
-    thread.start()
+    listener_thread = ListenerThread("listener-thread")
+    listener_thread.start()
 
-
-voiceRecognizer = sr.Recognizer()
-voiceRecognizer.non_speaking_duration = 0.1
-voiceRecognizer.pause_threshold = 0.1
-voiceRecognizer.phrase_threshold = 0.1
+def write_text(text):
+    f = codecs.open(path, "a", "utf-8")
+    f.write(str(text).upper()+'\n')
+    f.close()
 
 create_threads()
-
-while True:
-    with sr.Microphone() as source:
-        audio_text = voiceRecognizer.listen(source)
-
-        try:
-            subtitles = voiceRecognizer.recognize_google(audio_text, language="ru-RU",show_all=True)
-
-        except:
-            t = 1
